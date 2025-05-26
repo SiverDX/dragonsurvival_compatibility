cc8c7c67ea67ec9454d8b4383726d03bf80b2ce9
Kai <kai.zahn@yahoo.de>
2025-05-26 12:54:21 +0200

    fix cold sweat - update versions

--- src/main/java/de/cadentem/dragonsurvival_compatibility/mixin/cold_sweat/ClientMagicHUDHandlerMixin.java
+++ src/main/java/de/cadentem/dragonsurvival_compatibility/mixin/cold_sweat/ClientMagicHUDHandlerMixin.java
@@ -14,14 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ClientMagicHUDHandlerMixin {
     @Inject(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I", shift = At.Shift.BEFORE, ordinal = 0))
     private static void dragonsurvival_compatibility$renderExperienceBar1(final ForgeGui gui, final GuiGraphics guiGraphics, int width, final CallbackInfoReturnable<Boolean> callback) {
-        if (ClientConfig.COLD_SWEAT.get() && ClientSettingsConfig.getInstance().isCustomHotbarLayout()) {
+        if (ClientConfig.COLD_SWEAT.get() && ClientSettingsConfig.USE_CUSTOM_HOTBAR_LAYOUT.get()) {
             guiGraphics.pose().translate(0.0D, 4.0D, 0.0D);
         }
     }
 
     @Inject(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I", shift = At.Shift.AFTER, ordinal = 4))
     private static void dragonsurvival_compatibility$renderExperienceBar2(final ForgeGui gui, final GuiGraphics guiGraphics, int width, final CallbackInfoReturnable<Boolean> callback) {
-        if (ClientConfig.COLD_SWEAT.get() && ClientSettingsConfig.getInstance().isCustomHotbarLayout()) {
+        if (ClientConfig.COLD_SWEAT.get() && ClientSettingsConfig.USE_CUSTOM_HOTBAR_LAYOUT.get()) {
             guiGraphics.pose().translate(0.0D, -4.0D, 0.0D);
         }
     }
